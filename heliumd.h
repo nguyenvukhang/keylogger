50153485a334aff81548ace1fbc8d2a0d4872861
nguyenvukhang <brew4k@gmail.com>
2024-06-09 10:24:38 +0800

    Track global key state

--- heliumd.h
+++ heliumd.h
@@ -1,19 +1,26 @@
 #ifndef __KEYLOGGER_H__
 #define __KEYLOGGER_H__
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <time.h>
-#include <string.h>
 #include <ApplicationServices/ApplicationServices.h>
 #include <Carbon/Carbon.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 // https://developer.apple.com/library/mac/documentation/Carbon/Reference/QuartzEventServicesRef/Reference/reference.html
 
 FILE *logfile = NULL;
 // const char *logfileLocation = "/Users/khang/files/keystroke.log";
 const char *logfileLocation = "/dev/stdout";
 
-CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void*);
+CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void *);
 const char *convertKeyCode(int, bool, bool);
+
+struct KeyState {
+  bool cmd;
+  bool opt;
+  bool shift;
+  bool ctrl;
+} global_key_state;
 
 #endif
